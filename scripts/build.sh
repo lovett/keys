6be712812ca77c9d05c17ea5317dba693c28b015
Bill Lovett <bill@ilovett.com>
2026-07-06 11:20:15 -0400

    Simplify build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,4 @@
 
 set -eu
 
-cd "$(dirname "$0")/../"
-
-case "${1:-default}" in
-    --help)
-        echo "Compiles the application."
-        ;;
-    default)
-        go build
-        ;;
-    *)
-        echo "Unknown argument." >&2
-        ;;
-esac
+go build
