bdd948d9c257dc4aba1c6eff3846e7b5a88c0544
Bill Lovett <bill@ilovett.com>
2026-07-06 11:36:40 -0400

    Add --assumeyes flag to dnf install during setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,5 +7,5 @@ PACKAGES="alsa-lib-devel golangci-lint"
 # shellcheck disable=SC2086 # because splitting of PACKAGES is intentional.
 if ! rpm -q $PACKAGES >/dev/null 2>&1; then
     echo "Installing packages..."
-    sudo dnf install $PACKAGES
+    sudo dnf --assumeyes install $PACKAGES
 fi
