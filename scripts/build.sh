f758534d5bccc89bda06e6583311498109726f44
Bill Lovett <bill@ilovett.com>
2026-07-06 12:16:54 -0400

    Populate version asset during build.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,13 @@ set -eu
 
 cd "$(dirname "$0")/../"
 
+VERSION=$(date +"%Y.%m.%d+%H:%M:%S")
+VERSION_FILE="internal/asset/assets/version.txt"
+
+echo "$VERSION" > "$VERSION_FILE"
+
 sh scripts/setup.sh
 
 go build
+
+git checkout -q "$VERSION_FILE"
