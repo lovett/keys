a27b68044561886b2ab5376c02247a27f85e307d
Bill Lovett <bill@ilovett.com>
2026-07-15 15:33:03 -0400

    Refactor test script for brevity.

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,31 +7,21 @@ COVERAGE_DIR="${COVERAGE_DIR-/srv/www/coverage}"
 # Run from repository root
 cd "$(dirname "$0")/../"
 
-run_test() {
-    go test -timeout 1m ./... -coverprofile coverage.out "$@"
+if [ "${1-}" = "--help" ]; then
+    echo "Run unit tests."
+    echo ""
+    echo "Flags are passed on to 'go test'. See 'go help test' for details."
+    exit
+fi
+
+if [ -z "$COVERAGE_DIR" ]; then
+    go test -failfast -timeout 1m ./... "$@"
+else
+    COVERAGE_FILE="$COVERAGE_DIR/keys.html"
+    go test -failfast -timeout 1m ./... -coverprofile coverage.out "$@"
     grep -v -e "testhelper.go" -e "main.go" coverage.out > coverage_filtered.out
     mv coverage_filtered.out coverage.out
-    go tool cover -html=coverage.out -o=$COVERAGE_DIR/keys.html
-    echo "Wrote coverage file to $COVERAGE_DIR/keys.html"
-}
 
-case "${1:-default}" in
-    --help)
-        echo "Run unit tests."
-        echo ""
-        echo "Flags:"
-        echo "  --verbose or -v: Pass -v to go test"
-        ;;
-    -v)
-        run_test "-v"
-        ;;
-    --verbose)
-        run_test "-v"
-        ;;
-    default)
-        run_test
-        ;;
-    *)
-        echo "Unknown argument." >&2
-        ;;
-esac
+    go tool cover -html=coverage.out -o="$COVERAGE_FILE"
+    echo "Wrote coverage file to $COVERAGE_FILE"
+fi
