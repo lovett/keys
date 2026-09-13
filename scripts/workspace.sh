8339b67b3b79449a9c6406c476104a97aa85b4bf
Bill Lovett <bill@ilovett.com>
2026-09-13 18:52:03 -0400

    Simplify workspace script.

--- scripts/workspace.sh
+++ scripts/workspace.sh
@@ -1,30 +1,21 @@
 #!/usr/bin/env sh
 
+# Create a tmux workspace.
+
+SESSION_NAME="keys"
+
 set -eu
 
 cd "$(dirname "$0")/../"
 
-workspace() {
-    SESSION_NAME="keys"
-	tmux new-session -d -s "$SESSION_NAME" "$SHELL"
-	tmux send-keys -t "$SESSION_NAME" "$EDITOR ." C-m
+tmux new-session -d -s "$SESSION_NAME" "$SHELL"
 
-	tmux new-window -a -t "$SESSION_NAME" "$SHELL"
+tmux send-keys -t "$SESSION_NAME" "$EDITOR ." C-m
 
-	tmux new-window -a -t "$SESSION_NAME" -n "server" "scripts/watch.sh"
+tmux new-window -a -t "$SESSION_NAME" "$SHELL"
 
-	tmux select-window -t "$SESSION_NAME":0
-	tmux attach-session -t "$SESSION_NAME"
-}
+tmux new-window -a -t "$SESSION_NAME" -n "server" "scripts/watch.sh"
 
-case "${1:-default}" in
-    --help)
-        echo "Create a tmux workspace."
-        ;;
-    default)
-        workspace
-        ;;
-    *)
-        echo "Unknown argument." >&2
-        ;;
-esac
+tmux select-window -t "$SESSION_NAME":0
+
+tmux attach-session -t "$SESSION_NAME"
