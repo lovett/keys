90d3ca380eead77b3e9c0c8ca87c6ff8f55ab496
Bill Lovett <bill@ilovett.com>
2026-09-13 18:56:00 -0400

    Simplify watch script.

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env sh
 
+# Run the application and auto restart when files change.
+
 set -eu
 
 cd "$(dirname "$0")/../"
 
-case "${1:-default}" in
-    --help)
-        echo "Run the application and auto restart when files change."
-        ;;
-    default)
-        killall -q keys || true
-        find ./internal -type f | entr -r go run . start
-        ;;
-    *)
-        echo "Unknown argument." >&2
-        ;;
-esac
+killall -q keys || true
+
+find ./internal -type f | entr -r go run . start
