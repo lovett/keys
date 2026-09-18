a855d3702c3e5fc118bec2a7662c147c98f40d31
Bill Lovett <bill@ilovett.com>
2026-09-18 12:13:55 -0400

    Bump bun image to v1.4.2

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
-BUN_IMAGE="docker.io/oven/bun:alpine"
+BUN_IMAGE="docker.io/oven/bun:1.4.2-alpine"
 export BUN="podman run --rm -v $PWD:/app:Z -w /app $BUN_IMAGE bun"
+
 BIOME_IMAGE="ghcr.io/biomejs/biome"
 export BIOME="podman run --rm -v $PWD:/app:Z -w /app $BIOME_IMAGE"
+
 VACUUM_IMAGE="docker.io/dshanley/vacuum"
 export VACUUM="podman run --rm -v $PWD:/work:Z $VACUUM_IMAGE lint internal/asset/assets/openapi.yaml"
